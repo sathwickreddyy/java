ccdcea0243ea3d7cfda300d9baccc8729d809289
Sathwick reddy <sathwickreddyy@outlook.com>
2025-02-13 19:18:25 +0530

    Initialise Spring Integration Project

---
+++ Java_SpringIntegration/src/main/java/com/java/java_springintegration/JavaSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.java.java_springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringIntegrationApplication.class, args);
+    }
+
+}
---
+++ Java_SpringIntegration/src/test/java/com/java/java_springintegration/JavaSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.java_springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
