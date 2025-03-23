d62981ee696ff0e8c846e7e96b62b90df3f5ca2b
Sathwick reddy <sathwickreddyy@outlook.com>
2025-03-24 01:48:42 +0530

    Fixed Generate Proto Issue

--- Java_LLD_OOPS/src/main/java/com/java/pubsub/orderprocessing/pubsub/OrderPublisher.java
+++ Java_LLD_OOPS/src/main/java/com/java/pubsub/orderprocessing/pubsub/OrderPublisher.java
@@ -1,10 +1,19 @@
 package com.java.pubsub.orderprocessing.pubsub;
 
+import com.java.pubsub.orderprocessing.protobuf.OrderProto.Order;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderPublisher {
 
-//    private final PubSubTemplate pubSubTemplate;
+    private final PubSubTemplate pubSubTemplate;
 
+    public OrderPublisher(PubSubTemplate pubSubTemplate) {
+        this.pubSubTemplate = pubSubTemplate;
+    }
+
+    public void publishOrder(String topicName, Order order) {
+
+    }
 }
