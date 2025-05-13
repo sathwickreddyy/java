297e98fce05de137191e62ba017620a7132fc2a5
Sathwick reddy <sathwickreddyy@outlook.com>
2025-05-13 14:55:38 +0530

    Init Elevator Control System

---
+++ Java_LLD_OOPS/ElevatorControlSystem/src/main/java/com/java/lld/oops/elevatorcontrolsystem/ElevatorControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java.lld.oops.elevatorcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevatorControlSystemApplication.class, args);
+    }
+
+}
---
+++ Java_LLD_OOPS/ElevatorControlSystem/src/test/java/com/java/lld/oops/elevatorcontrolsystem/ElevatorControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.lld.oops.elevatorcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
