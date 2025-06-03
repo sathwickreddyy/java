1dce7d0409ad79406b828bd2d18bcea1c05398dd
Sathwick reddy <sathwickreddyy@outlook.com>
2025-06-03 18:13:49 +0530

    Adding Config Driven Data Loading Sub Module/Project

---
+++ JAVA_LLD_OOPS/ConfigDrivenDataLoading/src/main/java/com/java/lld/oops/configdriven/dataloading/ConfigDrivenDataLoadingApplication.java
@@ -0,0 +1,13 @@
+package com.java.lld.oops.configdriven.dataloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDrivenDataLoadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigDrivenDataLoadingApplication.class, args);
+    }
+
+}
---
+++ JAVA_LLD_OOPS/ConfigDrivenDataLoading/src/test/java/com/java/lld/oops/configdriven/dataloading/ConfigDrivenDataLoadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.lld.oops.configdriven.dataloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDrivenDataLoadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
