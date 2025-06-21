d487fef72c0587fab2248d8606cb5cf91fc3fab3
Sathwick reddy <sathwickreddyy@outlook.com>
2025-06-22 00:37:57 +0530

    Adding necessary annotations in main

--- JAVA_LLD_OOPS/ConfigDrivenDataLoading/src/main/java/com/java/lld/oops/configdriven/dataloading/ConfigDrivenDataLoadingApplication.java
+++ JAVA_LLD_OOPS/ConfigDrivenDataLoading/src/main/java/com/java/lld/oops/configdriven/dataloading/ConfigDrivenDataLoadingApplication.java
@@ -2,8 +2,14 @@ package com.java.lld.oops.configdriven.dataloading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAsync
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableTransactionManagement
 public class ConfigDrivenDataLoadingApplication {
 
     public static void main(String[] args) {
